daf5ca5bae06eb3b79c0094109786c530a71ddd8
piyush <piyushsachdeva29nov@gmail.com>
2026-05-01 16:48:38 +0530

    Route frontend API through ingress

--- frontend/lib/api.js
+++ frontend/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:5000/api";
+const API_URL = "/api";
 
 export const api = async (endpoint, method = "GET", body, token) => {
   const response = await fetch(`${API_URL}${endpoint}`, {
